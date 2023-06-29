83e381dc8b5fb7142495c767e83bcc19c2ec89c1
Robert Sachunsky <38561704+bertsky@users.noreply.github.com>
2023-06-29 20:33:07 +0000

    mehr tesserocr-API

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,6 @@
 sudo add-apt-repository ppa:alex-p/tesseract-ocr
 sudo apt-get update
 sudo apt-get install -y libtesseract-dev libleptonica-dev tesseract-ocr
-sudo apt-get install tesseract-ocr-script-frak tesseract-ocr-script-latn tesseract-ocr-eng tesseract-ocr-deu
+sudo apt-get install -y tesseract-ocr-script-frak tesseract-ocr-script-latn tesseract-ocr-eng tesseract-ocr-deu
 pip install ocrd
 pip install ocrd_tesserocr
