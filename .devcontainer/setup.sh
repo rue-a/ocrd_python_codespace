be8b4cd8af1889a22e3eeee0fe1977b4849b6044
Arne Ruemmler <arne.ruemmler@gmail.com>
2023-06-29 16:40:57 +0200

    fix typo

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,6 @@
 
 sudo add-apt-repository ppa:alex-p/tesseract-ocr
 sudo apt-get update
-sudo apt-get -y libtesseract-dev libleptonica-dev tesseract-ocr
+sudo apt-get install -y libtesseract-dev libleptonica-dev tesseract-ocr
 pip install ocrd
 pip install ocrd_tesserocr
