ee096d27be303ac17ca0b27af4485f64e788d22c
Arne Ruemmler <arne.ruemmler@gmail.com>
2023-06-29 16:36:58 +0200

    update

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:alex-p/tesseract-ocr
 sudo apt-get update
-sudo apt-get -y install tesseract-ocr
+sudo apt-get -y libtesseract-dev libleptonica-dev tesseract-ocr
 pip install ocrd
 pip install ocrd_tesserocr
