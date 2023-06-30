c566e3c6e634abbb299695ad1d331bd9fe376d44
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2023-06-30 04:18:00 +0200

    saubere Zeilen-Ergebnisse, OCRD-API

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,4 +6,5 @@ sudo apt-get install -y libtesseract-dev libleptonica-dev tesseract-ocr
 sudo apt-get install -y tesseract-ocr-script-frak tesseract-ocr-script-latn tesseract-ocr-eng tesseract-ocr-deu
 pip install ocrd
 pip install ocrd_tesserocr
+export TESSDATA_PREFIX=$VIRTUAL_ENV/share/tessdata
 pip freeze > requirements.txt
