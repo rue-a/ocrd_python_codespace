d3d816c5efa22dbaf801e24f206d1b64e4c919f5
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2023-06-30 14:55:55 +0200

    mehr Renderer (aber hier ohne Output)

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,7 +4,8 @@ sudo add-apt-repository ppa:alex-p/tesseract-ocr
 sudo apt-get update
 sudo apt-get install -y libtesseract-dev libleptonica-dev tesseract-ocr
 sudo apt-get install -y tesseract-ocr-script-frak tesseract-ocr-script-latn tesseract-ocr-eng tesseract-ocr-deu
+sudo chmod o+w /usr/share/tesseract-ocr/4.00/tessdata
 pip install ocrd
 pip install ocrd_tesserocr
-export TESSDATA_PREFIX=$VIRTUAL_ENV/share/tessdata
+export TESSDATA_PREFIX=/usr/share/tesseract-ocr/4.00/tessdata
 pip freeze > requirements.txt
